b4601c4e00b7225fb379241cf128f95e0fca2020
metheany <metheany.thorn@gmail..com>
2020-11-18 08:24:33 +0700

    bug: allow cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,6 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
@@ -54,5 +50,8 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 return $app;
