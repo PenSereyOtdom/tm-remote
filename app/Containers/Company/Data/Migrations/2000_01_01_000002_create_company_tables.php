3876b101f5e405f40ca34e8a7da591f536683065
Tsuyoshi Takagi <nhomfinn@gmail.com>
2020-12-28 12:43:23 +0700

    feat: alter company_name -> unique

--- app/Containers/Company/Data/Migrations/2000_01_01_000002_create_company_tables.php
+++ app/Containers/Company/Data/Migrations/2000_01_01_000002_create_company_tables.php
@@ -14,7 +14,7 @@ class CreateCompanyTables extends Migration
         Schema::create('companies', function (Blueprint $table) {
 
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->unsignedInteger('user_id')->nullable();
             $table->unsignedInteger('default_zoom_user_type')->nullable();
 
